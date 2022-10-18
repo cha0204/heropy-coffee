477bde92862641d65db18e0de5147785e8631d3e
cha0204 <112532836+cha0204@users.noreply.github.com>
2022-10-18 11:07:48 +0900

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,74 @@
+/*P. 292 */ 
+const badgeEl = document.querySelector('.badges');
+const topBtnEl = document.querySelector('top_btn');
+
+window.addEventListener("scroll", function(){
+    console.log(window.scrollY); //스크롤값 확인하기
+    if(760<window.scrollY){
+        // badgeEl.style.display='none';
+        gsap.toString(badgeEl,0.6,{
+            opacity:0,
+            display:'none'
+        })
+    }else{
+        // badgeEl.style.display='block';
+        gsap.toString(topBtnEl,0.6,{
+            opacity:1,
+            // x:0
+        })
+    }
+ });
+
+/*gsap.to(요소,시간,속성)*/ 
+
+const fadeEl=document.querySelectorAll('.fadein');
+
+fadeEl.forEach(function(fadeEl,index){
+ gsap.to(fadeEl,1,{
+    delay:(index+1)*0.7,//0.7초씩 느리게 시간을 추가
+    opacity:1
+});
+});
+
+// 커피프로모션 토글아이콘 클릭시 박스 숨기고/나타내기
+const promotionEl = document.querySelector('section.promotion');
+const toggleBtnEl = document.querySelector('.toggle_btn');
+
+//토글버튼을 클릭하면 프로모션의 크기가 변한다.
+toggleBtnEl.addEventListener('click',function(){
+
+    alert('top버튼 클릭 잘되요');
+    gsap.to(window,.6,{// 브라우저를 선택하여 0.6초만에 
+    scrollTo:0 //스크롤 꼭대기 0까지 올라간다. 
+    })
+    if(promotionEl.classList.contains('hide')){ //만약에 promotion클래스에 hide서식이 있다면
+        promotionEl.classList.remove('hide');//높이 663이 되어야
+    }else{ //그렇지 않으면
+        //높이가 0 
+        promotionEl.classList.add('hide');//높이663이되어야
+    }
+});
+
+// 6,7,8,10콘텐츠가 화면의 80%지점에 보일때 동작하는 스크립트
+const spyEl = document.querySelectorAll('section.scroll-spy');
+
+spyEl.forEach(function(spyEl){
+
+    new ScrollMagic
+    .Scene({//감시할 장면을 후가
+    triggerElement:spyEl,
+    triggerHook:0.8 //화면의 80%지엄에서 동작
+    })
+    .setClassToggle(spyEl, 'show') // show클래스를 적용하거나 해제하기
+    .addTo(new ScrollMagic.Controller()) //컨트롤러에 장면을 할당한다.
+});
+new Swiper('.awards .swiper',{
+    autoplay: true,
+    loop:true,
+    spaceBetween:30,
+    slidesPerView: 5, 
+    navigation: {
+        prevEl : '.awards .swiper-button-prev',
+        nextEl : '.awards .swiper-button-next'
+    }
+});
